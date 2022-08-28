27789a719a3cf758ddc438e68dff3d0f67eef624
David Kirowski <david.kiro@sgv.es>
2022-08-28 21:21:50 +0200

    added Jpa dependancy and databse

--- src/main/java/com/example/fullstack/student/StudentController.java
+++ src/main/java/com/example/fullstack/student/StudentController.java
@@ -1,29 +1,30 @@
 package com.example.fullstack.student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
+@AllArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
 
     @GetMapping
     public List<Student> getAllStudents() {
-        List<Student> students = Arrays.asList(
-                new Student(
-                        1L,
-                        "David",
-                        "david@gmail.com",
-                        Gender.MALE),
-                new Student(
-                        2L,
-                        "Lea",
-                        "lea@gmail.com",
-                        Gender.FEMALE)
-        );
-        return students;
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void addStudent(@RequestBody Student student) {
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(
+            @PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 }
